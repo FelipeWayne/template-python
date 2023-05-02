5a74785e695b33e6a895ef27bdd08354900f9f0a
Felipe Augusto <felipeaugustosilva94@gmail.com>
2023-05-01 22:52:30 -0300

    dynaconf para variaveis de ambiente

--- template_python/__init__.py
+++ template_python/__init__.py
@@ -1,1 +1,10 @@
 """apens um teste de pacote"""
+
+from dynaconf import settings as env
+
+
+# Carrega .env que define o ambiente Ex: development, production
+
+# log.debug(ENV['tt'])
+print(env.NUMBER)
+print(env.SENHA)
